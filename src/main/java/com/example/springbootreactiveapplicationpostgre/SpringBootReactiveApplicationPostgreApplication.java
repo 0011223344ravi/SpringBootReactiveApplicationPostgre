907eca4393d75ec066a54d002cc75f2cd5acd953
Ravi kumar gupta <0011223344ravi@gmail.com>
2023-01-03 13:18:39 +0530

    reactive crud app

---
+++ src/main/java/com/example/springbootreactiveapplicationpostgre/SpringBootReactiveApplicationPostgreApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactiveapplicationpostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactiveApplicationPostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactiveApplicationPostgreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootreactiveapplicationpostgre/SpringBootReactiveApplicationPostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactiveapplicationpostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactiveApplicationPostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
